863aca5bd4ada8b1bec7674a9a217011bb7103e9
Asvinie <Your Actual Email, In Quotes>
2018-11-08 11:46:27 +0100

    ajout de nogokiri

---
+++ app/services/start_scrap.rb
@@ -0,0 +1,26 @@
+class StartScrap
+  def initialize(url='https://coinmarketcap.com/all/views/all/')
+    @url = url
+    @page = Nokogiri::HTML(open(@url))
+  end
+
+  def perform
+
+    @prices = get_prices
+  end
+
+  def save
+  end
+
+  def get_prices
+    names = []
+    values = []
+    @page.css('//a[@class = "currency-name-container link-secondary"]').each do |node|
+      names << node.text
+    end
+    @page.css('//a[@class = "price"]').each do |node|
+      values << node.text
+    end
+    prices = Hash[names.zip(values)]
+    prices
+  end
