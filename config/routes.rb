5caaf30a762039f96c620ca54ba55a04b6f22e89
Asvinie <Your Actual Email, In Quotes>
2018-11-08 13:01:22 +0100

    page d'accueil

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-	root 'cryptos#index'
+
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #get '', to: 'cryptos#index'
+  get '/crypto', to: 'cryptos#index'
+  get '', to: 'home#index'
 end
